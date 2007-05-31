9c35a58662141e1a3632a36c7b3910d0c46373f1
gottmodus <gottmodus>
2007-05-31 08:35:13 +0000

    *** empty log message ***

---
+++ AjNet/include/config.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * This file set all default settings for AjNET
+ *
+ *
+ * PHP versions 5
+ *
+ * Copyright (C) 2007 Kersten Burkhardt
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @author     Kersten Burkhardt <kerstenk@gmail.com>
+ * @copyright  2007 Kersten Burkhardt
+ * @license    http://www.gnu.org/licenses/gpl.txt  GNU General Public License
+ * @version    CVS: $Id: config.inc.php,v 1.1 2007/05/31 08:35:13 gottmodus Exp $
+ * @since      File available since Release 0.0.1
+ */
+
+// {{{ constants
+
+/**
+ * Constants for AjNET.
+ */
+
+// MySQL connection settings
+define('MYSQL_USER', 'user');
+define('MYSQL_PASSWORD', 'pass');
+define('MYSQL_HOST', 'host');
+define('MYSQL_PORT', port);
+define('MYSQL_DATABASE', 'database');
+
+// }}}
+?>
